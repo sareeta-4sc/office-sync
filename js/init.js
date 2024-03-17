451946441b5509eacfa91b018ef603c4b5960b41
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-03-17 12:25:38 +0545

    file updated,change indeex to sign in page and responsive fixed

--- js/init.js
+++ js/init.js
@@ -1,38 +1,107 @@
-function _(el) {
-  return document.getElementById(el);
-}
+document.addEventListener("DOMContentLoaded", function() {
+    jQuery('.toggle-password').click(function(){
+        console.log('Toggle password clicked');
+        $(this).children().toggleClass('mdi-eye-outline mdi-eye-off-outline');
+        let input = $(this).prev();
+        console.log('Input field:', input);
+        input.attr('type', input.attr('type') === 'password' ? 'text' : 'password');
+        console.log('Input type:', input.attr('type'));
+    });
 
-function uploadFile() {
-  var file = _("file1").files[0];
-  // alert(file.name+" | "+file.size+" | "+file.type);
-  var formdata = new FormData();
-  formdata.append("file1", file);
-  var ajax = new XMLHttpRequest();
-  ajax.upload.addEventListener("progress", progressHandler, false);
-  ajax.addEventListener("load", completeHandler, false);
-  ajax.addEventListener("error", errorHandler, false);
-  ajax.addEventListener("abort", abortHandler, false);
-  ajax.open("POST", "file_upload_parser.php"); // http://www.developphp.com/video/JavaScript/File-Upload-Progress-Bar-Meter-Tutorial-Ajax-PHP
-  //use file_upload_parser.php from above url
-  ajax.send(formdata);
-}
+    var isAdvancedUpload = (function () {
+        var div = document.createElement("div");
+        return (
+            ("draggable" in div || ("ondragstart" in div && "ondrop" in div)) &&
+            "FormData" in window &&
+            "FileReader" in window
+        );
+    })();
 
-function progressHandler(event) {
-  _("loaded_n_total").innerHTML = "Uploaded " + event.loaded + " bytes of " + event.total;
-  var percent = (event.loaded / event.total) * 100;
-  _("progressBar").value = Math.round(percent);
-  _("status").innerHTML = Math.round(percent) + "% uploaded... please wait";
-}
+    let fileInput = document.querySelector(".default-file-input");
+    let uploadIcon = document.querySelector(".upload-icon");
+    let cannotUploadMessage = document.querySelector(".cannot-upload-message");
+    let uploadedFile = document.querySelector(".file-block");
+    let fileName = document.querySelector(".file-name");
+    let fileSize = document.querySelector(".file-size");
+    let progressBar = document.querySelector(".progress-bar");
+    let uploadButton = document.querySelector(".upload-button");
+    let fileFlag = 0;
 
-function completeHandler(event) {
-  _("status").innerHTML = event.target.responseText;
-  _("progressBar").value = 0; //wil clear progress bar after successful upload
-}
+    if (fileInput) {
+        fileInput.addEventListener("change", function(e) {
+            let file = e.target.files[0];
+            if (file) {
+                uploadIcon.innerHTML = "check";
+                fileName.textContent = file.name;
+                fileSize.textContent = (file.size / 1024).toFixed(1) + " KB";
+                uploadedFile.style.display = "flex";
+                progressBar.style.width = "0";
+                fileFlag = 0;
+            }
+        });
+    }
 
-function errorHandler(event) {
-  _("status").innerHTML = "Upload Failed";
-}
+    if (uploadButton) {
+        uploadButton.addEventListener("click", function() {
+            if (fileInput.value && fileFlag === 0) {
+                fileFlag = 1;
+                let width = 0;
+                let id = setInterval(function() {
+                    if (width >= 390) {
+                        clearInterval(id);
+                        uploadButton.innerHTML = `<span class="material-icons-outlined upload-button-icon">check</span> Uploaded`;
+                    } else {
+                        width += 5;
+                        progressBar.style.width = width + "px";
+                    }
+                }, 50);
+            } else {
+                cannotUploadMessage.style.display = "flex";
+            }
+        });
+    }
 
-function abortHandler(event) {
-  _("status").innerHTML = "Upload Aborted";
-}
+    let cancelAlertButton = document.querySelector(".cancel-alert-button");
+    if (cancelAlertButton) {
+        cancelAlertButton.addEventListener("click", function() {
+            cannotUploadMessage.style.display = "none";
+        });
+    }
+
+    if (isAdvancedUpload) {
+        let draggableFileArea = document.querySelector(".drag-file-area");
+
+        if (draggableFileArea) {
+            ["drag", "dragstart", "dragend", "dragover", "dragenter", "dragleave", "drop"].forEach(function(evt) {
+                draggableFileArea.addEventListener(evt, function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                });
+            });
+
+            ["dragover", "dragenter"].forEach(function(evt) {
+                draggableFileArea.addEventListener(evt, function(e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    uploadIcon.innerHTML = "file_download";
+                });
+            });
+
+            draggableFileArea.addEventListener("drop", function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+                uploadIcon.innerHTML = "check";
+                document.querySelector(".label").innerHTML = `<span class="browse-files"><span class="successful-message">File Dropped Successfully!</span> </span>`;
+                uploadButton.innerHTML = `Upload`;
+
+                let files = e.dataTransfer.files;
+                fileInput.files = files;
+                fileName.innerHTML = files[0].name;
+                fileSize.innerHTML = (files[0].size / 1024).toFixed(1) + " KB";
+                uploadedFile.style.display = "flex";
+                progressBar.style.width = "0";
+                fileFlag = 0;
+            });
+        }
+    }
+});
